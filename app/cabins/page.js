d0f9b5b0e9f16983aa147c45c0190fafbbfc743e
vsbron <vsbron@gmail.com>
2024-07-25 15:13:23 +0300

    Organized the file structure before truly startin gthe project

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,23 +1,9 @@
-import Counter from "../components/Counter";
+import Counter from "@/app/_components/Counter";
 
-async function Page() {
-  // Fetching data straight from the server component
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
-  // Logging on server
-  console.log(data);
-
+function Page() {
   return (
     <>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-
-      <Counter users={data} />
     </>
   );
 }
