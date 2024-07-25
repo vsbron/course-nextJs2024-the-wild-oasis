536c16eb1ede135f1170f047e0902b0b307de974
vsbron <vsbron@gmail.com>
2024-07-25 16:29:06 +0300

    Added custom font for the app

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
 
+// Setting the font
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
@@ -15,7 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
