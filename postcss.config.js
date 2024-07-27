8cb4af43cdb447ad0081e9ad6cb68d348e0a7a31
vsbron <vsbron@gmail.com>
2024-07-27 21:15:16 +0300

    Built Cabins and About pages. Added 2 versions of responsive images using <Image /> tag

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+/*
+/** @type {import('postcss-load-config').Config} */
+/*const config = {
   plugins: {
     tailwindcss: {},
   },
 };
-
-export default config;
+*/
