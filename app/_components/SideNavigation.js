35a63aca90f00feed06f132cf38289d318012021
vsbron <vsbron@gmail.com>
2024-07-30 17:00:25 +0300

    Current route now higlighted in the sidenav by using the usePathname custom hook

--- app/_components/SideNavigation.js
+++ app/_components/SideNavigation.js
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -26,13 +29,19 @@ const navLinks = [
 ];
 
 function SideNavigation() {
+  // Getting the pathname from custom hook
+  const pathname = usePathname();
+
+  // Returned JSX
   return (
     <nav className="border-r border-primary-900">
       <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 ${
+                pathname === link.href && "bg-primary-900"
+              }`}
               href={link.href}
             >
               {link.icon}
