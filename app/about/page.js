b5f2765785e2d6889503d6b18d6e4312e0dd2a03
vsbron <vsbron@gmail.com>
2024-07-30 15:37:25 +0300

    Added a totla cabins number fetch with revalidation ot the about page as a Challenge

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
+import { getCabins } from "../_lib/data-service";
 
 import image1 from "@/public/about-1.jpg";
 
+// Setting the revalidation time of the page to once a day
+export const revalidate = 86400;
+
+// Setting the meta title
 export const metadata = {
   title: "About",
 };
 
-function Page() {
+async function Page() {
+  // Fetching the cabins data from the Database
+  const cabins = await getCabins();
+
   return (
     <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
       <div className="col-span-3">
@@ -23,10 +31,10 @@ function Page() {
             and enjoying simple pleasures with family.
           </p>
           <p>
-            Our 8 luxury cabins provide a cozy base, but the real freedom and
-            peace you&apos;ll find in the surrounding mountains. Wander through
-            lush forests, breathe in the fresh air, and watch the stars twinkle
-            above from the warmth of a campfire or your hot tub.
+            Our {cabins.length} luxury cabins provide a cozy base, but the real
+            freedom and peace you&apos;ll find in the surrounding mountains.
+            Wander through lush forests, breathe in the fresh air, and watch the
+            stars twinkle above from the warmth of a campfire or your hot tub.
           </p>
           <p>
             This is where memorable moments are made, surrounded by
