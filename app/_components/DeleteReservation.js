aa0d3c3dfc046fba155c4f21d02125aa0de11a59
vsbron <vsbron@gmail.com>
2024-08-03 14:14:18 +0300

    Added useTransition hook to DeleteReservation component to see the deletion process status

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,19 +1,36 @@
 "use client";
-
+import { useTransition } from "react";
 import { TrashIcon } from "@heroicons/react/24/solid";
+
 import { deleteReservation } from "../_lib/actions";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
+  // Getting the transition status and startTransition function from the hook
+  const [isPending, startTransition] = useTransition();
+
+  // Delete handler
+  function handleDelete() {
+    if (confirm("Are you sure you want to delete this reservation?"))
+      startTransition(() => deleteReservation(bookingId));
+  }
+
   // Returned JSX
   return (
     <button
-      onClick={() => {
-        deleteReservation(bookingId);
-      }}
+      onClick={handleDelete}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      {!isPending ? (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      ) : (
+        <span className="mx-auto">
+          <SpinnerMini />
+        </span>
+      )}
     </button>
   );
 }
