5e8ceb86a2bf296a57ba4dd09e2bbab468ea39f6
vsbron <vsbron@gmail.com>
2024-08-05 15:26:26 +0300

    Added responsive support to About page

--- app/about/page.js
+++ app/about/page.js
@@ -16,8 +16,8 @@ async function Page() {
   const cabins = await getCabins();
 
   return (
-    <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
-      <div className="col-span-3">
+    <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center lg:grid-cols-1 lg:gap-y-12">
+      <div className="col-span-3 lg:col-span-1">
         <h1 className="text-4xl mb-10 text-accent-400 font-medium">
           Welcome to The Wild Oasis
         </h1>
@@ -44,7 +44,7 @@ async function Page() {
         </div>
       </div>
 
-      <div className="col-span-2">
+      <div className="col-span-2 lg:col-span-1 lg:mb-24 max-w-[600px]">
         <Image
           src={image1}
           placeholder="blur"
@@ -53,7 +53,7 @@ async function Page() {
         />
       </div>
 
-      <div className="relative aspect-square col-span-2">
+      <div className="relative aspect-square col-span-2 lg:col-span-1 lg:order-1 max-w-[600px]">
         <Image
           src="/about-2.jpg"
           fill
@@ -62,7 +62,7 @@ async function Page() {
         />
       </div>
 
-      <div className="col-span-3">
+      <div className="col-span-3 lg:col-span-1">
         <h1 className="text-4xl mb-10 text-accent-400 font-medium">
           Managed by our family since 1962
         </h1>
@@ -86,7 +86,7 @@ async function Page() {
           <div>
             <a
               href="/cabins"
-              className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+              className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all lg:mt-0"
             >
               Explore our luxury cabins
             </a>
