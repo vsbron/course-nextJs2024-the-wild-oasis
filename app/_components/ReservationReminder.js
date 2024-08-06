c8939405e9a0b085a01c634f7ddb3ac69ef490dc
vsbron <vsbron@gmail.com>
2024-08-06 11:42:17 +0300

    Added responsive support for the reminder component

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -13,9 +13,9 @@ function ReservationReminder() {
 
   // Returned JSX
   return (
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
-      <p>
-        <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{" "}
+    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 font-semibold shadow-xl shadow-slate-900 flex gap-6 items-center w-auto sm:py-3 sm:px-6">
+      <p className="w-[280px] xs:w-[200px] xs:text-sm">
+        <span>👋</span> Don&apos;t forget to reserve your dates <br className="xs:hidden" /> from{" "}
         {format(new Date(range.from), "MMM dd yyyy")} to{" "}
         {format(new Date(range.to), "MMM dd yyyy")}
       </p>
